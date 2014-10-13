64662f9d83039f22a128ece273540e74a31acdcb
Casey White <casey.r.white@gmail.com>
2014-10-13 12:47:11 -0700

    initial commit

---
+++ PhotoFilters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoFilters
+//
+//  Created by Casey R White on 10/13/14.
+//  Copyright (c) 2014 casecode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
