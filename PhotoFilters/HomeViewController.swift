c6c1673cf5ff0dea02684633b33852a44ee248d9
Casey White <casey.r.white@gmail.com>
2014-10-13 12:59:31 -0700

    set up home scene

--- PhotoFilters/HomeViewController.swift
+++ PhotoFilters/HomeViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mainPhotoImageView: UIImageView!
 
+
+    @IBAction func choosePhoto(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +27,5 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
